97fb8ac43bd3ba12f3fe3296b1012e60f04d9b1f
swchoi06 <swchoi06@wafflestudio.com>
2014-02-22 14:25:54 +0100

    Initial Commit with simple login; devise, omniauth-facebook gem used

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Share::Application.config.secret_key_base = '68fb800b901f863612b30f140f7bea5a464e5781c3fcf568659e37424555309492e299ed81a55485e41756253ccdb3054c76d334bc84c58b50c901aca0adb868'
